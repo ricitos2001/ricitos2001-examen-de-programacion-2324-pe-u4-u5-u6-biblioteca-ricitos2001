ebd5364ae5f767bdab34b41d100c789de3ad6b21
ricitos2001 <cgucha@iesrafaelalberti.es>
2024-03-14 18:47:31 +0100

    Ejercicio-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,17 @@
 import EstadoLibro.*
 
-data class Libros(var id: String, var titulo: String, var autor: String, var anioDePublicacion: String, var tematica: String, var estado: EstadoLibro = DISPONIBLE)
+// clase de datos Libros
+data class Libros(var id: String, var titulo: String, var autor: String, var anioDePublicacion: Int, var tematica: String, var estado: EstadoLibro = DISPONIBLE)
 
+// clase enumerada para el estado del libro
 enum class EstadoLibro {
     DISPONIBLE, PRESTADO
 }
-
+// clase para gestionar la libreria
 class GestorDeBiblioteca {
     var catalogo = mutableListOf<Libros>()
+    private var registroPrestamos = mutableListOf<Libros>()
+    // metodo para agregar libros
     fun agregarLibro(libro: Libros):String {
         if (!catalogo.contains(libro)) {
             catalogo.add(libro)
@@ -18,6 +22,7 @@ class GestorDeBiblioteca {
             return "el libro ya esta en el catalogo"
         }
     }
+    // metodo para eliminar libros
     fun eliminarLibro(id: String):String {
         val libro = catalogo.find { it.id == id }
         if (libro != null) {
@@ -28,57 +33,56 @@ class GestorDeBiblioteca {
             return "el libro no encontrado por título"
         }
     }
-    fun cambiarDisponibilidad(titulo: String, nuevoEstado: EstadoLibro) {
-        catalogo.find { it.titulo == titulo }?.let {
-            it.estado = nuevoEstado
-            println("estado cambiado a $nuevoEstado para el libro: ${it.titulo}")
+    // metodo para
+    fun registrarPrestamo(id: String):String {
+        val libro = catalogo.find { it.id == id }
+        if (libro != null && libro.estado == DISPONIBLE) {
+            libro.estado = PRESTADO
+            registroPrestamos.add(libro)
+            return "el libro  ha sido prestado"
+        } else {
+            return "el libro no esta disponible para prestamo."
         }
     }
-    fun consultarDisponibilidad(id: String):Boolean {
-        return catalogo.any { it.id == id && it.estado == DISPONIBLE }
+    //metodo para devolver un libro
+    fun devolverLibro(id: String):String {
+        val libro = catalogo.find { it.id == id }
+        if (libro != null && libro.estado == PRESTADO) {
+            libro.estado = DISPONIBLE
+            registroPrestamos.remove(libro)
+            return "el libro  ha sido devuelto"
+        } else {
+            return "el libro aun no ha sido prestado"
+        }
     }
-
-    fun mostrarEstado(catalogo: String):String {
-        return catalogo
+    //metodo para consultar la disponibilidad de un libro
+    fun consultarDisponibilidad(id: String): Libros? {
+        return catalogo.find { it.id == id && it.estado == DISPONIBLE }
+    }
+    // metodo para mostrar el estado de un libro
+    fun mostrarEstado() {
+        catalogo.forEach {
+            println("${it.titulo}: ${it.estado}")
+        }
     }
 }
-
-class RegistrosDePrestamos(private var gestorDeLibros: GestorDeBiblioteca) {
-    fun registrarPrestamo(titulo: String): String {
-        if (gestorDeLibros.consultarDisponibilidad(titulo)) {
-            gestorDeLibros.cambiarDisponibilidad(titulo, PRESTADO)
-            return "el libro $titulo ha sido prestado"
-        } else {
-            return "el libro $titulo no esta disponible para prestamo."
-        }
-    }
-    fun devolverLibro(titulo: String):String {
-        if (gestorDeLibros.consultarDisponibilidad(titulo)) {
-            gestorDeLibros.cambiarDisponibilidad(titulo, DISPONIBLE)
-            return "el libro $titulo ha sido prestado"
-        } else {
-            return "el libro $titulo aun no ha sido prestado"
-        }
-    }
-}
-
+// funcion principal que inicia el programa
 fun main() {
     val gestorDeBiblioteca = GestorDeBiblioteca()
-    val libro1 = Libros("1234567A", "El señor de los anillos", "J.R.R. Tolkien", "11/11/2001", "fantasia", DISPONIBLE)
-    val libro2 = Libros("1234567B", "Harry Potter", "J.K. Rowling", "11/11/2001", "Fantasia", PRESTADO)
-    val libro3 = Libros("1234567C", "El Libro del buen humor", "cesar gabriel ucha sousa", "11/11/2024", "humor", DISPONIBLE)
+    val libro1 = Libros("1234567A", "El señor de los anillos", "J.R.R. Tolkien",1954 , "fantasia", DISPONIBLE)
+    val libro2 = Libros("1234567B", "Harry Potter", "J.K. Rowling", 1998, "Fantasia", PRESTADO)
+    val libro3 = Libros("1234567C", "El Libro del buen humor", "Ramon Gomez", 2020, "humor", DISPONIBLE)
     gestorDeBiblioteca.agregarLibro(libro1)
     gestorDeBiblioteca.agregarLibro(libro2)
     gestorDeBiblioteca.agregarLibro(libro3)
     println(gestorDeBiblioteca.catalogo)
     println(gestorDeBiblioteca.eliminarLibro("1234567C"))
     println(gestorDeBiblioteca.consultarDisponibilidad(libro1.estado.toString()))
-    val registroDePrestamos = RegistrosDePrestamos(gestorDeBiblioteca)
     println("\nrealizar registros de prestamos:")
-    println(registroDePrestamos.registrarPrestamo(libro1.id)) // libro1 prestado
-    println(registroDePrestamos.registrarPrestamo(libro2.id)) // libro2 ya prestado
+    println(gestorDeBiblioteca.registrarPrestamo(libro1.id)) // libro1 prestado
+    println(gestorDeBiblioteca.registrarPrestamo(libro2.id)) // libro2 ya prestado
     println("\nrealizar devoluciones:")
-    println(registroDePrestamos.devolverLibro(libro1.id)) // libro1 devuelto
-    println(registroDePrestamos.devolverLibro(libro2.id)) // libro2 ya devuelto
-    println(gestorDeBiblioteca.mostrarEstado("${gestorDeBiblioteca.catalogo}"))
+    println(gestorDeBiblioteca.devolverLibro(libro1.id)) // libro1 devuelto
+    println(gestorDeBiblioteca.devolverLibro(libro2.id)) // libro2 ya devuelto
+    gestorDeBiblioteca.mostrarEstado()
 }
